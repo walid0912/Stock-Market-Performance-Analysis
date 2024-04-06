cc043a306233f91d1b094c1544684576a50a4a26
Walid KROUCHI <117538373+walid0912@users.noreply.github.com>
2024-04-07 00:12:27 +0200

    Update Stock_Market_Performance_Analysis.py

--- Stock_Market_Performance_Analysis.py
+++ Stock_Market_Performance_Analysis.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
 
+start_date = datetime.now() - pd.DateOffset(months=3)
+end_date = datetime.now()
+
+tickers = ['AAPL', 'MSFT', 'NFLX', 'GOOG']
+
+df_list = []
+
+for ticker in tickers:
+    data = yf.download(ticker, start=start_date, end=end_date)
+    df_list.append(data)
+
+df = pd.concat(df_list, keys=tickers, names=['Ticker', 'Date'])
+print(df.head())
